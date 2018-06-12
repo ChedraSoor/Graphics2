e3d81e53421fb738ae60c94c05003ffbccea4d5e
Chedra Soor <cvsoor@student.fullsail.edu>
2018-06-12 14:03:08 -0400

    +Draw some stuff .h

--- Graphics2Project/Graphics2Project/LoadObj.h
+++ Graphics2Project/Graphics2Project/LoadObj.h
@@ -4,4 +4,25 @@
 
 using namespace std;
 
-bool LoadOBJ(const char* paths, vector<glm::vec3> & out_Vert, vector<glm::vec2> & out_UV, vector<glm::vec3> & out_Normals)
+class Loader
+{
+public:
+	Loader();
+	~Loader();
+
+	vector< unsigned int > vertexIndices, uvIndices, normalIndices;
+	vector< glm::vec3 > tmp_vertices;
+	vector< glm::vec2 > tmp_uvs;
+	vector< glm::vec3 > tmp_normals;
+
+	bool LoadOBJ(const char* paths, vector<glm::vec3> & out_Vert, vector<glm::vec2> & out_UV, vector<glm::vec3> & out_Normals);
+
+};
+
+Loader::Loader()
+{
+}
+
+Loader::~Loader()
+{
+}
